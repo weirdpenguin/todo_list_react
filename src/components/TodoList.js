51457075bc4467779f959f64d976ee953c0d0aae
Irina Kononenko <selezneva@xaker.ru>
2020-11-19 14:18:10 +0300

    add checkbox

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 class TodoList extends React.Component {
-
     render() {
         const todos = this.props.todos;
-
         const todosList = todos.map((todo) =>
             <li key={todo.id.toString()}>
+                <input type='checkbox' />
                 {todo.text}
                 <button
                     onClick={(event) => this.props.deleteTodo(todo.id)}>
-                    <i className='fas fa-eraser'></i>
+                        <i className='fas fa-eraser'></i>
                 </button>
             </li>
-        )
+        );
 
-        return ( 
+        return (
             <ul>{todosList}</ul>
         );
     }
